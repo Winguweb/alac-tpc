63ec2d828a7a51c29dbd6f79feb252bd6efef328
rockarloz <rockarlos@me.com>
2019-09-06 13:42:26 -0500

    add some query for gl

--- alac-dashboard/db/migrate/20190329174201_create_actors.rb
+++ alac-dashboard/db/migrate/20190329174201_create_actors.rb
@@ -3,18 +3,15 @@ class CreateActors < ActiveRecord::Migration[5.1]
     create_table :actors, id: :uuid do |t|
       t.string   :name
       t.string   :last_name
-      t.string   :individual_actor_type
-      t.string   :individual_actor_cat_1
-      t.string   :individual_actor_cat_2
-      t.string   :colective_actor_type
-      t.string   :colective_actor_cat_1
-      t.string   :colective_actor_cat_2
+      t.string   :general_type
+      t.string   :type
+      t.string   :subtype
+      t.string   :subtype_2
       t.string   :institution
       t.string   :public_branch
       t.string   :position
       t.string   :entity
       t.boolean  :public_auth
-      t.boolean  :no_name
       t.boolean  :status
       t.timestamps null: false
     end
