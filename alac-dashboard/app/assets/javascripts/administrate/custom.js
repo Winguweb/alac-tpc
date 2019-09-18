f74b8475bead57b4b5936ef6f92428c1c835524e
Florencia Lafuente <lafuente.florencia@gmail.com>
2019-09-18 10:43:17 -0300

    Show custom charge input

--- alac-dashboard/app/assets/javascripts/administrate/custom.js
+++ alac-dashboard/app/assets/javascripts/administrate/custom.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
+
+  // Show charge field when individual actor type is selected
   $('.actors-type-checkbox').click(function(){
     var inputValue = $(this).attr('value');
     if (inputValue === 'individual') {
       $('.actor-charge-field').removeClass('d-none');
     } else {
       $('.actor-charge-field').addClass('d-none');
+    };
+  });
+
+  $('.actor-charge-select').on('change', function() {
+    const selectValue = this.value;
+    if (selectValue === 'otro') {
+      $('.actor-charge-input').removeClass('d-none');
+    } else {
+      $('.actor-charge-input').addClass('d-none');
     };
   });
 });
