93caab8fddf686200ea8130c0dd8750aa58f9ba2
gus <gustavo@winguweb.org>
2019-09-27 18:07:01 +0200

    add toggle select input in tools modal

--- alac-dashboard/app/assets/javascripts/administrate/custom.js
+++ alac-dashboard/app/assets/javascripts/administrate/custom.js
@@ -52,4 +52,12 @@ $(document).ready(function(){
         $('.has-tool-input').removeClass('d-none');
       }
     });
+
+    $('.entity_tools_select').on('change', function(){
+      if(this.value == ''){
+        $('.entity_text').removeClass('hide')
+      }else{
+        $('.entity_text').addClass('hide')
+      }
+    })
 });
