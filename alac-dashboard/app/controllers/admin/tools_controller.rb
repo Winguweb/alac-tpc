f06d31e115633864c32cb545f6d5bd189a2ae78b
rockarloz <rockarlos@me.com>
2019-10-15 13:37:08 -0500

    flow for update docs to s3

--- alac-dashboard/app/controllers/admin/tools_controller.rb
+++ alac-dashboard/app/controllers/admin/tools_controller.rb
@@ -14,7 +14,28 @@ module Admin
     # def find_resource(param)
     #   Tool.find_by!(slug: param)
     # end
+    def create
+      # @too = Tool.new()
+      # @too.advisory_id = tool_params[:advisory_id]
+      # @too.tools_used = tool_params[:tools_used]
+      # @too.go_entity = tool_params[:go_entity]
+      # @too.entity = tool_params[:entity]
+      # @too.radication_date = tool_params[:radication_date]
+      # @too.deadline = tool_params[:deadline]
+      # @too.have_answer = tool_params[:have_answer]
+      # @too.answer_date = tool_params[:answer_date]
+      # binding.pry
+      # @too.document = tool_params[:document]
+     
+      @tool = Tool.new(tool_params)
+      if @tool.save
+        redirect_to "/admin/reports/#{Tool.last.advisory.characterization.case_id}"
+      end
+    end
 
+    def tool_params
+      params.fetch(:tool, {}).permit(:advisory_id, :tools_used, :go_entity,:entity, :radication_date, :deadline, :have_answer, :answer_date, :document)
+    end
     # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
     # for more information
   end
