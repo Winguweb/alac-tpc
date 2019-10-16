8cba34a416639b2e54198c23543a93302f26ee20
rockarloz <rockarlos@me.com>
2019-10-16 10:19:05 -0500

    remove comments and console.log

--- alac-dashboard/app/controllers/admin/tools_controller.rb
+++ alac-dashboard/app/controllers/admin/tools_controller.rb
@@ -15,18 +15,6 @@ module Admin
     #   Tool.find_by!(slug: param)
     # end
     def create
-      # @too = Tool.new()
-      # @too.advisory_id = tool_params[:advisory_id]
-      # @too.tools_used = tool_params[:tools_used]
-      # @too.go_entity = tool_params[:go_entity]
-      # @too.entity = tool_params[:entity]
-      # @too.radication_date = tool_params[:radication_date]
-      # @too.deadline = tool_params[:deadline]
-      # @too.have_answer = tool_params[:have_answer]
-      # @too.answer_date = tool_params[:answer_date]
-      # binding.pry
-      # @too.document = tool_params[:document]
-     
       @tool = Tool.new(tool_params)
       if @tool.save
         redirect_to "/admin/reports/#{Tool.last.advisory.characterization.case_id}"
