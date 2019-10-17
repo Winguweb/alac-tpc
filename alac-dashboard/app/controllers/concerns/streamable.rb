70768ba8a8e3429fae563a045b371baf726aa18c
rockarloz <rockarlos@me.com>
2019-10-17 09:57:31 -0500

    add method for get some keys

--- alac-dashboard/app/controllers/concerns/streamable.rb
+++ alac-dashboard/app/controllers/concerns/streamable.rb
@@ -14,29 +14,37 @@ module Streamable
       reports.each do | report|
         if characterization = Characterization.where(case_id: report[0]).first
           elements = get_report_detail(report[0])
+          
+          hecho =  elements[0]
           municipio_reporte = elements[16]
-          municipio_residente = elements[20]
+          if elements[20][0] == "¿A cuál organización pertenece?"
+            municipio_residente = elements[19]
+          else
+            municipio_residente = elements[20]
+          end
           haseahble = []
           elements.each do |answer|
             haseahble.push(answer[0])
             haseahble.push(answer[1])
           end
+          
           char_data =  Hash[*haseahble]
+          final_keys = char_data.keys - ["Edad/Género","Edad","Genero","¿Cuál es el presunto hecho de corrupción que  quieres reportar?","Departamento del Hecho","¿Con el fin de formalizar su denuncia, estaría dispuesto (a) a acudir ante la autoridad competente para denunciar el hecho de presunta corrupción para el cual está solicitando nuestra asesoría?","¿Ha denunciado ante alguna autoridad o algún medio de comunicación el hecho que está reportando?","Adjuntar Evidencias","Nombre(s)","Apellido(s)","Correo Electrónico","Teléfono de Contacto","Nivel de escolaridad","Ocupación","Tipo de Persona","Departamento de Residencia","¿Es usted lider social o defensor de derechos humanos?","Terminos","¿Por qué?","¿Cual?","¿A cuál organización pertenece?"] 
           sheet << [
             report[0],
             get_info_report(report[0])[0][0],
             get_info_report(report[0])[0][1],
-            char_data["¿Cuál es el presunto hecho de corrupción que  quieres reportar?"],
+            hecho,
             characterization.scope,
             format_arr(characterization.affected_sector),
             format_arr(characterization.rights_violated),
             format_arr(characterization.kind_corruption),
             format_arr(characterization.affected_area),
             format_arr(characterization.kind_responsability),
-            char_data["Departamento del Hecho"],
+            char_data[final_keys[1]],
             municipio_reporte,
-            char_data["Departamento de Residencia"],
-            municipio_residente = elements[20],
+            char_data[final_keys[2]],
+            municipio_residente,
             char_data["Edad"],
             char_data["Ocupación"],
             char_data["¿Es usted lider social o defensor de derechos humanos?"],
@@ -44,10 +52,11 @@ module Streamable
             char_data["¿Cual?"],
             char_data["¿Con el fin de formalizar su denuncia, estaría dispuesto (a) a acudir ante la autoridad competente para denunciar el hecho de presunta corrupción para el cual está solicitando nuestra asesoría?"],
             char_data["¿Por qué?"],
-            char_data["¿Pertenece usted a alguna de las siguientes poblaciones? ¿Cual?"]
+            char_data[final_keys[0]]
           ]
         end
       end
+      char_data["Municipios de Residencia - #{char_data["Departamento de Residencia"]}"]
 
       # Characterization.all.each do |characterization|
       #   sheet << [characterization.scope,characterization.affected_sector,characterization.rights_violated,characterization.kind_corruption,characterization.affected_area,characterization.kind_responsability]
@@ -81,7 +90,7 @@ module Streamable
     xlsx.write_worksheet 'Asesoría Legal' do |sheet|
       sheet << ["ID del Caso","ID de la Asesoría","Fecha de Seguimiento ALAC","Tipo de respuesta que se dio al reportante","Cuadro resumen de la actuación"]
       Advisory.all.each do |advisory|
-        sheet << [advisory.characterization.case_id,advisory.id, advisory.tracking_date,advisory.kind_answer,advisory.summary]
+        sheet << [advisory.characterization.case_id,advisory.id, advisory.tracking_date,format_arr(advisory.kind_answer),advisory.summary]
       end
     end
 
