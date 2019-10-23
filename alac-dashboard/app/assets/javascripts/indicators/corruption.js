57dd306ed0a3266ee515d5bb84a599f5cd93b0d7
Florencia Lafuente <lafuente.florencia@gmail.com>
2019-10-23 13:29:29 -0300

    Style corruption graph [ch1997]

--- alac-dashboard/app/assets/javascripts/indicators/corruption.js
+++ alac-dashboard/app/assets/javascripts/indicators/corruption.js
@@ -14,11 +14,19 @@ fetch(`${URL}/api/indicators/corruption`)
 function drawChart(data){
   let keys = []
   let chartData = []
+  let colorBars = []
   data.map( d => {
     const key = Object.keys(d)[0]
+    const color = data.indexOf(d) % 2 === 0 ? '#5472d2' : '#00c1cf'
+
     keys.push(key)
     chartData.push(d[key])
+    colorBars.push(color)
   })
+
+  
+
+
 
   const ctx = document.getElementById('corruption-chart').getContext('2d');
   new Chart(ctx, {
@@ -26,20 +34,36 @@ function drawChart(data){
     data: {
         labels: keys,
         datasets: [{
-            label: '# of Votes',
             data: chartData,
-            backgroundColor: 'rgba(54, 162, 235, 0.8)',
-            borderColor: 'rgba(54, 162, 235, 0.8)',
+            backgroundColor: colorBars,
+            borderColor: colorBars,
             borderWidth: 1
         }]
     },
     options: {
+      legend: {
+        display: false,
+        labels: {
+          fontSize: 28
+        }
+      },
+      tooltips: {
+        titleFontSize: 28,
+        bodyFontSize: 28
+      },
         scales: {
             yAxes: [{
                 ticks: {
+                    fontSize: 28,
                     beginAtZero: true
                 }
-            }]
+            }],
+            xAxes: [{
+              ticks: {
+                  fontSize: 28,
+                  fontStyle: 'bold',
+              }
+          }]
         }
     }
 });
