1e8ba29ba005efeb08bc795b709300a945b5f354
Florencia Lafuente <lafuente.florencia@gmail.com>
2019-10-23 14:15:11 -0300

    Fix ambit viz [ch1997]

--- alac-dashboard/app/assets/javascripts/indicators/ambit.js
+++ alac-dashboard/app/assets/javascripts/indicators/ambit.js
@@ -13,10 +13,14 @@ fetch(`${URL}/api/indicators/ambit`)
 function drawChart(data){
   let keys = []
   let chartData = []
+  let colorBars = []
+
   data.map( d => {
     const key = Object.keys(d)[0]
+    const color = data.indexOf(d) % 2 === 0 ? '#5472d2' : '#00c1cf'
     keys.push(key)
     chartData.push(d[key])
+    colorBars.push(color)
   })
 
   const ctx = document.getElementById('ambit-chart').getContext('2d');
@@ -27,24 +31,36 @@ function drawChart(data){
         datasets: [{
             label: '# of Votes',
             data: chartData,
-            backgroundColor: 'rgba(54, 162, 235, 0.8)',
-            borderColor: 'rgba(54, 162, 235, 0.8)',
+            backgroundColor: colorBars,
+            borderColor: colorBars,
             borderWidth: 1
         }]
     },
     options: {
-        scales: {
+      legend: {
+        display: false,
+        labels: {
+          fontSize: 24
+        }
+      },
+      tooltips: {
+        titleFontSize: 24,
+        bodyFontSize: 24
+      },
+      scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero: true
+                    beginAtZero: true,
+                    fontSize: 26
                 }
             }],
             xAxes: [{
               ticks: {
-                beginAtZero: true
+                beginAtZero: true,
+                fontSize: 26
               }
             }]
-        }
+      }
     }
 });
 }
