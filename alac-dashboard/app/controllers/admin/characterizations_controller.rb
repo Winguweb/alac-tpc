14b3581d427c716630fe6e379e499abd6fc46748
gus <gustavo@winguweb.org>
2019-11-28 14:15:45 -0300

    kick off filters [ch2131]

--- alac-dashboard/app/controllers/admin/characterizations_controller.rb
+++ alac-dashboard/app/controllers/admin/characterizations_controller.rb
@@ -1,20 +1,29 @@
 module Admin
   class CharacterizationsController < Admin::ApplicationController
     include MainHelper
+    include SelectHelper
+    helper_method :characterization_form_options, :corruption_options, :rights_violated
     # To customize the behavior of this controller,
     # you can overwrite any of the RESTful actions. For example:
     #
-    # def index
-    #   super
-    #   @resources = Characterization.
-    #     page(params[:page]).
-    #     per(10)
-    # end
+    def index
+      super
+      allowed_params = allowed_index_params
+      binding.pry
+        @resources = Characterization.
+          page(params[:page]).
+          per(10)
+    end
 
     # Define a custom finder by overriding the `find_resource` method:
     # def find_resource(param)
     #   Characterization.find_by!(slug: param)
     # end
+
+    def allowed_index_params
+      allowed_params = params.permit(:status, :scope, :kind_corruption, :rights_violated)
+      allowed_params.reject{ |_, v| v.blank? }
+    end
 
     # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
     # for more information
