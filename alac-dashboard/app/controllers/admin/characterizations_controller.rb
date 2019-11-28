dcdc8b7b74a8c4dafe46864f3fe1dea0b22a8f57
gus <gustavo@winguweb.org>
2019-11-28 16:34:18 -0300

    add filters of status and scope [ch2131]

--- alac-dashboard/app/controllers/admin/characterizations_controller.rb
+++ alac-dashboard/app/controllers/admin/characterizations_controller.rb
@@ -12,21 +12,35 @@ module Admin
       @scope = allowed_params[:scope]
       @kind_corruption = allowed_params[:kind_corruption]
       @rights_violated = allowed_params[:rights_violated]
+      @characterizations = Characterization.all
+      @resources = characterization_finder
+      @resources.page(params[:page]).per(10)
       super
-        @resources = Characterization.
-          page(params[:page]).
-          per(10)
+
     end
 
-    # Define a custom finder by overriding the `find_resource` method:
-    # def find_resource(param)
-    #   Characterization.find_by!(slug: param)
-    # end
+    private
 
     def allowed_index_params
       allowed_params = params.permit(:status, :scope, :kind_corruption, :rights_violated)
       allowed_params.reject{ |_, v| v.blank? }
     end
+
+    def characterization_finder
+      @characterizations_filter = @characterizations
+      @characterizations_filter = filter_by_status if @status
+      @characterizations_filter = filter_by_scope if @scope
+      @characterizations_filter
+    end
+
+    def filter_by_status
+      @characterizations_filter.where(status: @status)
+    end
+
+    def filter_by_scope
+      @characterizations_filter.where(scope: @scope)
+    end
+
 
     # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
     # for more information
